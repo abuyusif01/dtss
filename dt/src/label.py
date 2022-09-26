1e5d7452a545ed5376ec9708785e5b487c75ae5c
abuyusif01 <abubakaryusuf951@gmail.com>
2022-09-27 00:29:07 +0800

    dtss: initial commit
    Signed-off-by: abuyusif01 <abubakaryusuf951@gmail.com>

---
+++ dt/src/label.py
@@ -0,0 +1,43 @@
+##### Script to label generated dataset at level1; uses threshold condition to detect command injection attacks and network DoS attacks#####
+##### Results are labelled dataset stored as 'data1.csv' in logs folder ######
+##### Level 2 labelling takes this data1.csv as input and label the remaining attacks wrt attack durations #####
+from utils.utils import SENSOR2_THRESH
+import csv
+
+tank_lb = 0.3
+tank_ub = 5.81
+bottle_ub = 0.9
+
+with open("logs/data.csv", "r") as csvinput:
+    with open("logs/data1.csv", "w") as csvoutput:
+        writer = csv.writer(csvoutput)
+        for row in csv.reader(csvinput):
+            if row[0] == "timestamp":
+                writer.writerow(row + ["class"])
+            else:
+                # Handle all anomalous case in here; and mark anything that is outside these cases as Normal.                                                                                                               ############################################################################################################################
+                ### For command injection attacks
+                ### Anomaly when motor valve closed when bottle_ll < bottle_ub and when tank_ll > tank_lb
+                ### Anomaly when motor valve is open if bottle_ll >= bottle_ub or if flowlevel >= sensor2_threshold or if tank_ll <= tank_ll
+                ############################################################################################################################
+                if int(row[4]) == 1 and (
+                    float(row[1]) <= tank_lb
+                    or (float(row[2]) >= SENSOR2_THRESH and float(row[2]) != 999)
+                    or (float(row[3]) >= bottle_ub and float(row[3]) != 999)
+                ):
+                    writer.writerow(row + ["Command Injection"])
+                elif (
+                    int(row[4]) == 0
+                    and float(row[3]) < bottle_ub
+                    and float(row[1]) > tank_lb
+                ):
+                    writer.writerow(row + ["Command Injection"])
+                ################################################################################
+                ### For Network DoS: MitM attacks as well as Network DoS: TCP SYN flood attacks
+                ### Anomaly if flowlevel or bottle_ll is marked as 999
+                ################################################################################
+                elif float(row[2]) == 999 or float(row[3]) == 999:
+                    writer.writerow(row + ["Network DoS"])
+                # Final case
+                else:
+                    writer.writerow(row + ["Normal"])
---
+++ dt/src/plc1.py
@@ -0,0 +1,191 @@
+"""
+
+todo: might need to edit this file (Abubakar)
+FP plc1.py
+"""
+
+from minicps.devices import PLC
+from utils.utils import PLC1_DATA, PLC1_PROTOCOL, PLC1_ADDR, STATE
+from utils.utils import PLC2_ADDR, PLC3_ADDR
+from utils.utils import PLC_PERIOD_SEC  # PLC_SAMPLES
+from utils.utils import TANK_M, BOTTLE_M, SENSOR2_THRESH
+from utils.utils import SENSOR1, ACTUATOR1
+from utils.utils import SENSOR2_1, SENSOR2_2, SENSOR3_1, SENSOR3_3
+import time
+import logging
+import csv
+import datetime
+import os.path
+
+# tag addresses
+
+
+class FPPLC1(PLC):
+
+    # boot process
+    def pre_loop(self, sleep=0.1):
+        print("DEBUG: FP PLC1 enters pre_loop")
+        print()
+
+        time.sleep(sleep)
+
+    def store_values(
+        self, liquidlevel_tank, flowlevel, liquidlevel_bottle, motor_status, count
+    ):
+        with open("logs/data.csv", "a+") as writeobj:
+            fieldnames = [
+                "timestamp",
+                "tank_liquidlevel",
+                "flowlevel",
+                "bottle_liquidlevel",
+                "motor_status",
+            ]
+            csv_writer = csv.DictWriter(writeobj, fieldnames=fieldnames)
+            if count == 0:
+                csv_writer.writeheader()
+                # count = 1
+
+            csv_writer.writerow(
+                {
+                    "timestamp": str(datetime.datetime.now()),
+                    "tank_liquidlevel": liquidlevel_tank,
+                    "flowlevel": flowlevel,
+                    "bottle_liquidlevel": liquidlevel_bottle,
+                    "motor_status": motor_status,
+                }
+            )
+
+    def main_loop(self):
+        """plc1 main loop.
+        - reads values from sensors
+        - drives actuator according to the control strategy
+        - updates its enip server
+        - logs the control strategy events (info, exceptions)
+        """
+
+        print("DEBUG: FP PLC1 enters main_loop.")
+        print
+        # FYI: BSD-syslog format (RFC 3164), e.g. <133>Feb 25 14:09:07 webserver syslogd: restart   PRI <Facility*8+Severity>, HEADER (timestamp host), MSG (program/process message)
+        logging.basicConfig(
+            filename="logs/plc1.log",
+            format="%(levelname)s %(asctime)s "
+            + PLC1_ADDR
+            + " %(funcName)s %(message)s",
+            datefmt="%m/%d/%Y %H:%M:%S",
+            level=logging.DEBUG,
+        )
+
+        count = 0
+        while True:
+            # get sensor1 value
+            liquidlevel_tank = float(
+                self.get(SENSOR1)
+            )  # physical process simulation (sensor 1 reads value)
+            print(
+                "DEBUG PLC1 - liquid level of tank (SENSOR 1): %.5f" % liquidlevel_tank
+            )
+            self.send(
+                SENSOR1, liquidlevel_tank, PLC1_ADDR
+            )  # network process simulation (value of sensor 1 is stored as enip tag)
+
+            if liquidlevel_tank <= TANK_M["LowerBound"]:
+                print(
+                    "INFO PLC1 - liquid level of tank (SENSOR 1) under LowerBound: %.2f <= %.2f -> close mv (ACTUATOR 1)."
+                    % (liquidlevel_tank, TANK_M["LowerBound"])
+                )
+                logging.info(
+                    "Liquid level of tank (SENSOR 1) under LowerBound: %.2f <= %.2f -> close mv (ACTUATOR 1)."
+                    % (liquidlevel_tank, TANK_M["LowerBound"])
+                )
+                self.set(ACTUATOR1, 0)  # CLOSE actuator mv
+                self.send(ACTUATOR1, 0, PLC1_ADDR)
+
+            # read from PLC2
+            try:
+                # estimated_flowlevel = float(self.get(SENSOR2_2))
+                flowlevel = float(self.receive(SENSOR2_2, PLC2_ADDR))
+                print("DEBUG PLC1 - receive flowlevel (SENSOR 2): %f" % flowlevel)
+                self.send(SENSOR2_1, flowlevel, PLC1_ADDR)
+
+                if flowlevel >= SENSOR2_THRESH:
+                    print(
+                        "INFO PLC1 - Flow level (SENSOR 2) over SENSOR2_THRESH:  %.2f >= %.2f -> close mv (ACTUATOR 1)."
+                        % (flowlevel, SENSOR2_THRESH)
+                    )
+                    logging.info(
+                        "Flow level (SENSOR 2) over SENSOR2_THRESH:  %.2f >= %.2f -> close mv (ACTUATOR 1)."
+                        % (flowlevel, SENSOR2_THRESH)
+                    )
+                    self.set(ACTUATOR1, 0)  # CLOSE actuator mv
+                    self.send(ACTUATOR1, 0, PLC1_ADDR)
+                else:
+                    logging.info(
+                        "Flow level (SENSOR 2) under SENSOR2_THRESH:  %.2f < %.2f -> leave mv status (ACTUATOR 1)."
+                        % (flowlevel, SENSOR2_THRESH)
+                    )
+            except:
+                logging.warning(
+                    "Flow level (SENSOR 2) is not received. Program is unable to proceed properly"
+                )
+                flowlevel = 999
+
+            # read from PLC3
+            try:
+                # estimated_liquidlevel_bottle = float(self.get(SENSOR3_3))
+                liquidlevel_bottle = float(self.receive(SENSOR3_3, PLC3_ADDR))
+                print(
+                    "DEBUG PLC1 - receive liquid level of bottle (SENSOR 3): %f"
+                    % liquidlevel_bottle
+                )
+                self.send(SENSOR3_1, liquidlevel_bottle, PLC1_ADDR)
+
+                if liquidlevel_bottle >= BOTTLE_M["UpperBound"]:
+                    print(
+                        "INFO PLC1 - Liquid level (SENSOR 3) over BOTTLE_M['UpperBound']:  %.2f >= %.2f -> close mv (ACTUATOR 1)."
+                        % (liquidlevel_bottle, BOTTLE_M["UpperBound"])
+                    )
+                    logging.info(
+                        "Liquid level (SENSOR 3) over BOTTLE_M['UpperBound']:  %.2f >= %.2f -> close mv (ACTUATOR 1)."
+                        % (liquidlevel_bottle, BOTTLE_M["UpperBound"])
+                    )
+                    self.set(ACTUATOR1, 0)  # CLOSE actuator mv
+                    self.send(ACTUATOR1, 0, PLC1_ADDR)
+
+                elif (
+                    liquidlevel_bottle < BOTTLE_M["UpperBound"]
+                    and liquidlevel_tank > TANK_M["LowerBound"]
+                ):
+                    print(
+                        "INFO PLC1 - Liquid level (SENSOR 3) under BOTTLE_M['UpperBound']: %.2f < %.2f ->  open mv (ACTUATOR 1)."
+                        % (liquidlevel_bottle, BOTTLE_M["UpperBound"])
+                    )
+                    logging.info(
+                        "Liquid level (SENSOR 3) under BOTTLE_M['UpperBound']: %.2f < %.2f -> open mv (ACTUATOR 1)."
+                        % (liquidlevel_bottle, BOTTLE_M["UpperBound"])
+                    )
+                    self.set(ACTUATOR1, 1)  # OPEN actuator mv
+                    self.send(ACTUATOR1, 1, PLC1_ADDR)
+            except:
+                logging.warning(
+                    "Liquid level (SENSOR 3) is not received. Program is unable to proceed properly"
+                )
+                liquidlevel_bottle = 999
+
+            motor_status = int(self.get(ACTUATOR1))
+            if os.path.isfile("trigger.txt"):
+                self.store_values(
+                    liquidlevel_tank, flowlevel, liquidlevel_bottle, motor_status, count
+                )
+                count = 1
+            time.sleep(PLC_PERIOD_SEC)
+            print("DEBUG FP PLC1 shutdown")
+
+
+if __name__ == "__main__":
+    plc1 = FPPLC1(
+        name="plc1",
+        state=STATE,
+        protocol=PLC1_PROTOCOL,
+        memory=PLC1_DATA,
+        disk=PLC1_DATA,
+    )

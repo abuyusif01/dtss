fbf58b27f2fde06f05025051d94aec00c49c9796
abuyusif01 <abubakaryusuf951@gmail.com>
2022-11-19 01:52:07 +0800

    api_logs: done with api
    Signed-off-by: abuyusif01 <abubakaryusuf951@gmail.com>

--- dt/fp/src/API_LOGS/app.py
+++ dt/fp/src/API_LOGS/app.py
@@ -8,31 +8,40 @@ app = Flask(__name__)
 
 
 class Utils:
-    # read file by line numbers
     """
     This certainly not the optimal way to do this, but it works.
     """
 
     def get_lines(fp, line_number):
-        return [x for i, x in enumerate(fp) if i == line_number ]
+        return [x for i, x in enumerate(fp) if i == line_number]
 
 
 # take a line number and return the line from the log file
-@app.route("/gen_table", methods=["GET"])
-def gen_table():
-    line_number = int(request.args.get("line_number"))
+@app.route("/get_data", methods=["GET"])
+def gen_table() -> Response:
+
+    values = request.args.to_dict()
+    line_number = int(values["line_number"])
+    file_name = values["file_name"]
+
     try:
-        def generate():
+        """
+        since we dont wanna have a broken pipe or typeError
+        we end up using generators to yeild the data as well as
+        safely catch all exceptions
+        """
+
+        def generate() -> Exception:
             try:
-                with open("api_log.csv", "r") as f:
-                    yield str(Utils.get_lines(f, line_number)).replace("\\n", "")
+                with open(file_name, "r") as f:
+                    yield str(Utils.get_lines(f, line_number))[2:-4]
             except Exception as e:
                 yield str(e)
 
-    except:
-        return "generate failed"
+    except Exception as e:
+        return e
     return Response(generate())
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000, host="localhost")
